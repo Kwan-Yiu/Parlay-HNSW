e29c5e1cc848eb09089201a014d0df43cf4137cd
AmuroEita <1071307515@qq.com>
2025-06-19 20:53:55 +0800

    debug

--- python/scripts/nyt_test.py
+++ python/scripts/nyt_test.py
@@ -6,7 +6,7 @@ NAME = "nytimes-256-angular"
 DATA_DIR = "data/" + NAME + "/"
 metric = "mips"
 
-# wp.build_vamana_index(metric, "float", DATA_DIR + NAME + "_base.fbin", DATA_DIR + "outputs/" + NAME, 150, 300, .85, True)
+wp.build_vamana_index(metric, "float", DATA_DIR + NAME + "_base.fbin", DATA_DIR + "outputs/" + NAME, 150, 300, .85, True)
 
 Index = wp.load_index(metric, "float", DATA_DIR + NAME + "_base.fbin", DATA_DIR + "outputs/" + NAME)
 
--- python/scripts/sift_test.py
+++ python/scripts/sift_test.py
@@ -8,7 +8,7 @@ metric = "Euclidian"
 
 # wp.build_vamana_index(metric, "float", DATA_DIR + NAME + "_base.fbin", DATA_DIR + "outputs/" + NAME + "_64", 64, 128, 1.15, True)
 # wp.build_vamana_index(metric, "float", DATA_DIR + NAME + "_base.fbin", DATA_DIR + "outputs/" + NAME + "_40", 40, 80, 1.15, True)
-# wp.build_vamana_index(metric, "float", DATA_DIR + NAME + "_base.fbin", DATA_DIR + "outputs/" + NAME + "_32", 32, 64, 1.15, True)
+wp.build_hnsw_index(metric, "float", DATA_DIR + NAME + "_base.fbin", DATA_DIR + "outputs/" + NAME + "_32", 32, 64, 1.15, True)
 
 Index = wp.load_index(metric, "float", DATA_DIR + NAME + "_base.fbin", DATA_DIR + "outputs/" + NAME + "_64")
 
